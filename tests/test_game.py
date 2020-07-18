7d54bb0bd74b47f3bdeb22fa199c5b09dc1dcd9e
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2020-07-18 14:21:01 -0500

    add dimension check for cnn

---
+++ tests/test_game.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+
+import pytest
+import torch
+import csgo_wp
+
+
+class Test_CNN:
+
+    def test_dims_default(self):
+        mod = csgo_wp.CNNModel()
+
+        t = torch.rand(size=(5, 1, 10, 10))
+
+        result = mod(t)
+
+        assert result.shape == (5, 2)
