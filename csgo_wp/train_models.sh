a0914b3fd36d4553d3621464f170ded944fe1304
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2020-11-08 01:18:35 -0500

    train with channels transform

--- csgo_wp/train_models.sh
+++ csgo_wp/train_models.sh
@@ -1,28 +1,30 @@
-python train.py --n-epochs 10 >> training_log.log 2>&1
-python train.py --n-epochs 10 --learning-rate 0.00001 >> training_log.log 2>&1
-python train.py --n-epochs 10 --model-type cnn --learning-rate 0.00001 >> training_log.log 2>&1
-python train.py --n-epochs 10 --model-type res --learning-rate 0.00001 >> training_log.log 2>&1
-python train.py --n-epochs 10 --hidden-sizes 200,1000,500,200,50 >> training_log.log 2>&1
-python train.py --n-epochs 10 --learning-rate 0.00001 --hidden-sizes 200,1000,500,200,50 >> training_log.log 2>&1
-python train.py --n-epochs 10 --batch-size 2 >> training_log.log 2>&1
-python train.py --n-epochs 10 --batch-size 4 >> training_log.log 2>&1
-python train.py --n-epochs 10 --batch-size 8 >> training_log.log 2>&1
-python train.py --n-epochs 10 --batch-size 16 >> training_log.log 2>&1
-python train.py --n-epochs 10 --batch-size 32 >> training_log.log 2>&1
-python train.py --n-epochs 10 --batch-size 64 >> training_log.log 2>&1
-python train.py --n-epochs 10 --batch-norm True >> training_log.log 2>&1
-python train.py --n-epochs 10 --dropout True >> training_log.log 2>&1
-python train.py --n-epochs 10 --batch-norm True --dropout True >> training_log.log 2>&1
-python train.py --n-epochs 10 --model-type cnn --batch-norm True >> training_log.log 2>&1
-python train.py --n-epochs 10 --model-type cnn --dropout True >> training_log.log 2>&1
-python train.py --n-epochs 10 --model-type cnn --batch-norm True --dropout True >> training_log.log 2>&1
-python train.py --n-epochs 10 --model-type res --batch-norm True >> training_log.log 2>&1
-python train.py --n-epochs 10 --model-type res --dropout True >> training_log.log 2>&1
-python train.py --n-epochs 10 --model-type res --batch-norm True --dropout True >> training_log.log 2>&1
-python train.py --n-epochs 10 --activation LeakyReLU >> training_log.log 2>&1
-python train.py --n-epochs 10 --model-type cnn --activation LeakyReLU >> training_log.log 2>&1
-python train.py --n-epochs 10 --model-type res --activation LeakyReLU >> training_log.log 2>&1
-python train.py --n-epochs 10 --model-type cnn --cnn-options "1,3,3,1,0,2,1,0|3,1,3,1,0,2,1,0|1,1,3,1,0,2,1,0" >> training_log.log 2>&1
-python train.py --n-epochs 10 --model-type cnn --cnn-options "1,3,3,1,0,2,1,0|3,1,3,1,0,2,1,0" >> training_log.log 2>&1
-python train.py --n-epochs 100 >> training_log.log 2>&1
-python train.py --n-epochs 100 --learning-rate 0.00001 >> training_log.log 2>&1
+python train.py --transform channels --n-epochs 10 >> training_log.log 2>&1
+python train.py --transform channels --n-epochs 10 --learning-rate 0.00001 >> training_log.log 2>&1
+python train.py --transform channels --n-epochs 10 --model-type cnn --cnn-options "6,1,3,1,0,2,1,0" --learning-rate 0.00001 >> training_log.log 2>&1
+python train.py --transform channels --n-epochs 10 --model-type res --learning-rate 0.00001 >> training_log.log 2>&1
+python train.py --transform channels --n-epochs 10 --hidden-sizes 200,1000,500,200,50 >> training_log.log 2>&1
+python train.py --transform channels --n-epochs 10 --learning-rate 0.00001 --hidden-sizes 200,1000,500,200,50 >> training_log.log 2>&1
+python train.py --transform channels --n-epochs 10 --batch-size 2 >> training_log.log 2>&1
+python train.py --transform channels --n-epochs 10 --batch-size 4 >> training_log.log 2>&1
+python train.py --transform channels --n-epochs 10 --batch-size 8 >> training_log.log 2>&1
+python train.py --transform channels --n-epochs 10 --batch-size 16 >> training_log.log 2>&1
+python train.py --transform channels --n-epochs 10 --batch-size 32 >> training_log.log 2>&1
+python train.py --transform channels --n-epochs 10 --batch-size 64 >> training_log.log 2>&1
+python train.py --transform channels --n-epochs 10 --batch-norm True >> training_log.log 2>&1
+python train.py --transform channels --n-epochs 10 --dropout True >> training_log.log 2>&1
+python train.py --transform channels --n-epochs 10 --batch-norm True --dropout True >> training_log.log 2>&1
+python train.py --transform channels --n-epochs 10 --model-type cnn --cnn-options "6,6,5,1,0,1,1,0" >> training_log.log 2>&1
+python train.py --transform channels --n-epochs 10 --model-type cnn --cnn-options "6,1,3,1,0,2,1,0" --batch-norm True >> training_log.log 2>&1
+python train.py --transform channels --n-epochs 10 --model-type cnn --cnn-options "6,1,3,1,0,2,1,0" --dropout True >> training_log.log 2>&1
+python train.py --transform channels --n-epochs 10 --model-type cnn --cnn-options "6,1,3,1,0,2,1,0" --batch-norm True --dropout True >> training_log.log 2>&1
+python train.py --transform channels --n-epochs 10 --model-type res --batch-norm True >> training_log.log 2>&1
+python train.py --transform channels --n-epochs 10 --model-type res --dropout True >> training_log.log 2>&1
+python train.py --transform channels --n-epochs 10 --model-type res --batch-norm True --dropout True >> training_log.log 2>&1
+python train.py --transform channels --n-epochs 10 --activation LeakyReLU >> training_log.log 2>&1
+python train.py --transform channels --n-epochs 10 --model-type cnn --cnn-options "6,1,3,1,0,2,1,0" --activation LeakyReLU >> training_log.log 2>&1
+python train.py --transform channels --n-epochs 10 --model-type res --activation LeakyReLU >> training_log.log 2>&1
+python train.py --transform channels --n-epochs 10 --model-type cnn --cnn-options "6,3,3,1,0,2,1,0|3,1,3,1,0,2,1,0|1,1,3,1,0,2,1,0" >> training_log.log 2>&1
+python train.py --transform channels --n-epochs 10 --model-type cnn --cnn-options "6,3,3,1,0,2,1,0|3,1,3,1,0,2,1,0" >> training_log.log 2>&1
+python train.py --transform channels --n-epochs 10 --model-type cnn --cnn-options "6,6,1,1,0,1,1,0|6,6,5,1,0,1,1,0" >> training_log.log 2>&1
+python train.py --transform channels --n-epochs 100 >> training_log.log 2>&1
+python train.py --transform channels --n-epochs 100 --learning-rate 0.00001 >> training_log.log 2>&1
