68193598feeb114de99cfdacc69de401a873d337
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2020-11-15 12:25:46 -0500

    change default to false because apparently argparser's bool stuff doesn't work like i expected

--- csgo_wp/train.py
+++ csgo_wp/train.py
@@ -185,7 +185,7 @@ if __name__ == '__main__':
 
     parser.add_argument('--early-stopping',
                         type=bool,
-                        default=True,
+                        default=False,
                         )
 
     args = parser.parse_args()
